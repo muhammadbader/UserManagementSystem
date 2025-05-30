2fba7816c5f35a046391079363c83d280daaf115
mhmmd <muhammad@flare.watch>
2025-05-30 19:56:18 +0300

    cloudinary

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import cloudinary from "../../utils/cloudinary.js";
 
 import UserModel from "../../../DB/model/user.model.js";
 import auth from "../../middleware/auth.js";
@@ -35,14 +36,23 @@ router.delete("/:id", auth(), admin(), async (req, res) => {
   return res.status(200).json({ message: "User deleted successfully" });
 });
 
+// upload file, use in postman the form-data option inside Body tab
 router.put("/:id", auth(), fileUpload().single("image"), async (req, res) => {
-    // form data should be sent
-    const { id } = req.params;
-    const user = await UserModel.findByPk(id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    
+  // first approach
+  const { id } = req.params;
+  const user = await UserModel.findByPk(id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  // second approach: cloudinary
+  const {secure_url} = await cloudinary.uploader.upload(req.file.path);
+  user.profilePicture = secure_url;
+  await user.save();
+
+  return res.status(200).json({
+    message: "User updated successfully",
+  });
 });
 
 export default router;
--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,26 +1,34 @@
 import multer from "multer";
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
 
+// store the uploaded files in the "uploads" directory with a unique name
 function fileUpload() {
+  // alternative: cloudinary for file storage or S3 aws
+
+  // approach 2: cloudinary or S3 aws
   const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/");
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${nanoid()}-${file.originalname}`);
-    },
+    // // uncommect for the first approach, second approach is cloudinary or S3 aws which is in the put API
+    // destination: (req, file, cb) => {
+    //   cb(null, "uploads/");
+    // },
+    // filename: (req, file, cb) => {
+    //   cb(null, `${nanoid()}-${file.originalname}`);
+    // },
   });
 
-  filefilter = (req, file, cb) => {
+  function fileFilter(req, file, cb) {
     const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
     if (allowedTypes.includes(file.mimetype)) {
       cb(null, true);
     } else {
-      cb(new Error("Invalid file type. Only JPEG, PNG, and GIF are allowed."), false);
+      cb(
+        new Error("Invalid file type. Only JPEG, PNG, and GIF are allowed."),
+        false
+      );
     }
   }
 
-  const upload = multer({ filefilter, storage });
+  const upload = multer({ fileFilter, storage });
 
   return upload;
 }
