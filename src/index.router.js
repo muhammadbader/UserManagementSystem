46079542ad7e93389b7aac0576ae587426e889fd
mhmmd <muhammad@flare.watch>
2025-06-03 23:22:36 +0300

    cors

--- src/index.router.js
+++ src/index.router.js
@@ -4,10 +4,13 @@ import userRouter from "./modules/user/user.router.js";
 import authRouter from "./modules/auth/auth.router.js";
 import blogRouter from "./modules/blog/blog.router.js";
 
+import cors from "cors";
+
 
 const initApp = (app, express) => {
   connectDB();
 
+  app.use(cors());
   app.use(express.json());
   // in case you got /user in the endpoint, redirect to userRouter
   app.use("/users", userRouter);
